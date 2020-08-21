ca486dc749e7b74d5db233c64ce1e3f4e78e876b
Rachel Newman <rachel.newman@argos.co.uk>
2020-08-21 14:01:31 +0100

    add search bar and make head into a functional component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,7 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { SearchField, withClear } from '@jsluna/form'
+import { SearchButton } from '@jsluna/button'
 
 import { TogetherWeAreSainsburys } from '@jsluna/images'
 import {
@@ -7,34 +9,54 @@ import {
   Header as LunaHeader,
   HeaderLogo,
   HeaderActions,
+  HeaderItem,
 } from '@jsluna/react'
 import { Basket } from '@jsluna/icons'
 
 import "./Header.scss";
 
-class Header extends Component {
-  componentDidMount() {
+const ClearableSearchField = withClear(SearchField)
+
+const Header = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
     axios.get(`ENDPOINT_PLACEHOLDER`).then((res) => {
       console.log(res);
     });
+  }, [])
+
+  const handleSearch = (e) => {
+    if (searchTerm) console.log(searchTerm)
   }
 
-  render() {
-    return (
-      <LunaHeader>
-        <HeaderLogo>
-          <TogetherWeAreSainsburys className="header__logo" />
-          <span className="ln-u-visually-hidden">Together We Are Sainsburys</span>
-        </HeaderLogo>
-        <HeaderActions align="right" label="Basket">
-          <TextButton className="ln-u-pull-right">
-            <Basket /> 42
-            <span className="ln-u-visually-hidden">Your basket</span>
-          </TextButton>
-        </HeaderActions>
-      </LunaHeader>
-    );
-  }
+  return (
+    <LunaHeader>
+      <HeaderLogo>
+        <TogetherWeAreSainsburys className="header__logo" />
+        <span className="ln-u-visually-hidden">Together We Are Sainsburys</span>
+      </HeaderLogo>
+      <HeaderItem fullWidth align="center" label="Search">
+        <ClearableSearchField
+          name='search-bar'
+          hasButton
+          onChange = {(e) => setSearchTerm(e.target.value)}
+          button={<SearchButton
+            onClick={(e) => handleSearch(e)}
+            />}
+            className='header__search-bar'
+          />
+      </HeaderItem>
+
+      <HeaderActions align="right" label="Basket">
+        <TextButton className="ln-u-pull-right">
+          <Basket /> 42
+          <span className="ln-u-visually-hidden">Your basket</span>
+        </TextButton>
+      </HeaderActions>
+    </LunaHeader>
+  )
+
 }
 
 export default Header;
